540c15d70606581793699367f2f0166f5c65cd57
mike <michael.bree@gmail.com>
2019-04-17 19:48:18 -0600

    work in progress

--- bus/process_departure_data_file.py
+++ bus/process_departure_data_file.py
@@ -1,5 +1,6 @@
 import datetime
 import math
+import string
 
 from data_manager import dataman_factory
 from da_manager import DaData
@@ -28,6 +29,10 @@ class Runner(object):
         self._taps_per_date_dict = {}
         self._taps_per_day_dict = {}
 
+       # OBS  self._bad_stop_routes = {}
+       # OBS  self._good_stop_routes = {}
+
+        self._fare_type_dict = {}
         self._keep_iso_days = [1,2,3,4,5] # Monday - Friday
 
         self._iosday = ["Invalid", "Monday", "Tuesday", "Wednesday",
@@ -112,7 +117,7 @@ class Runner(object):
             5908, # TAPS: 337 NAME: Lawson Terminal I/B
         ]
 
-        return filter_stops
+        return
         """
         if self._dataset == DATASET.JUNE:
 
@@ -155,7 +160,12 @@ class Runner(object):
         return filter_stops
         """
 
+    def clean_info(self, s):
+
+        return "".join(c for c in s if c in string.ascii_letters)
+
     def run(self, filename):
+
 
         if filename.find('jan') > 0:
             self._dataset = DATASET.JUNE
@@ -166,10 +176,25 @@ class Runner(object):
         self._daman = DaData()
 
         runner.make_valid_stop_list()
+
+
+        self._skip_days = [
+            '2018-09-01',
+            '2018-09-02',
+            '2018-09-03',
+            '2018-09-08',
+            '2018-09-09',
+            '2018-09-15',
+            '2018-09-16',
+            '2018-09-22',
+            '2018-09-23',
+            '2018-09-29',
+            '2018-09-30',
+        ]
+
         filter_stops = self.get_filter_stops()
 
         expected_cols = None
-        zero_count = 0
         stop_count = 0
         bad_count = 0
         bad_route_count = 0
@@ -181,7 +206,7 @@ class Runner(object):
         valid_count = 0
         invalid_count = 0
 
-        f = open(filename)
+        f = open(filename, "r")
 
         count = 0
         for line in f:
@@ -202,88 +227,84 @@ class Runner(object):
                 print parts
                 continue
 
+            skip_line = False
+            invalid_stop_id = False
+
             info = parts[8].strip()
+            info = self.clean_info(info)
+            route = int(parts[4])
+            stop = int(parts[5])
+            date = parts[2]
+            t = parts[3]
 
             # Skip transfers all transfers
             if info.find('ansfer') > 0:
                 transfer_count += 1
-                print "SKIPPING TRANSFER"
+                # print "SKIPPING TRANSFER"
+                # skip_line = True
                 continue
-
-            route = int(parts[4])
 
             if route not in self._valid_route_ids:
                 bad_route_count += 1
                 # print "SKIPPING BAD ROUTE", bad_route_count
                 continue
 
-            route_data = self._taps_per_route_dict.get(route, {})
+            if stop in [0, 9999]:
+                invalid_stop_id = True
+                continue
 
+            if not stop in self._valid_stop_ids:
+                continue
+
+            if date in self._skip_days:
+                continue
+
+            # Examine the time
+            t_parts = t.split(":")
+            h = int(t_parts[0])
+            m = int(t_parts[1])
+            minutes = 60 * h + m
+            # print "TIME", h, m, minutes
+
+            if minutes < 15*60 or minutes > 18*60:
+                wrong_time_count += 1
+                continue
+
+                # print "SKIP INVALID TIME!!!!", wrong_time_count
+                # skip_line = True
+
+            route_data = self._taps_per_route_dict.get(route, {})
             tap_count = route_data.get('tap_count', 0)
-            tap_count += 1
             tap_count_err = route_data.get('err_count', 0)
 
-            # print "ROUTE", route
-
-            stop = int(parts[5])
-
-            bad_stop = False
-            if stop == 0:
-                zero_count += 1
-                bad_stop = True
-
-            if stop == 9999:
-                bad_count += 1
-                bad_stop = True
-
-            if bad_stop:
+            if invalid_stop_id:
                 tap_count_err += 1
+            else:
+                tap_count += 1
 
             route_data['tap_count'] = tap_count
             route_data['err_count'] = tap_count_err
             self._taps_per_route_dict[route] = route_data
 
-            if bad_stop:
-                continue
+            # Update fare type data
+            c = self._fare_type_dict.get(info, 0)
+            c += 1
+            self._fare_type_dict[info] = c
 
-            if stop in filter_stops:
-                filter_count += 1
-                continue
-
-            # Examine the date
-            date = parts[2]
-
+            # update taps per date dict
             c = self._taps_per_date_dict.get(date, 0)
             c += 1
             self._taps_per_date_dict[date] = c
 
             # print date
-
             date_object = self.get_date_object(date)
             isoweekday = date_object.isoweekday()
             day = self._iosday[isoweekday]
 
+            # update taps per day dict
             c = self._taps_per_day_dict.get(day, 0)
             c += 1
             self._taps_per_day_dict[day] = c
-
-            if isoweekday not in self._keep_iso_days:
-                wrong_day_count += 1
-                print "SKIP INVALID DAY", wrong_day_count
-                continue
-
-            # Examine the time
-            t = parts[3]
-            t_parts = t.split(":")
-            h = int(t_parts[0])
-            m = int(t_parts[1])
-            minutes = 60 * h + m
-#            print "TIME", h, m, minutes
-
-            if minutes < 6*60 or minutes > 9*60:
-                wrong_time_count += 1
-                print "SKIP INVALID TIME!!!!", wrong_time_count
-                continue
 
             stop_count += 1
 
@@ -295,8 +316,9 @@ class Runner(object):
             # print "BUS PASS NO", bus_pass
 
             if bus_pass is None:
+                pass
                 # print "SKIP NO BAS PASS"
-                continue
+                # skip_line = True
 
             if stop in self._valid_stop_ids:
                 valid_count += 1
@@ -328,16 +350,15 @@ class Runner(object):
         f.close()
 
 
-        print "Read %d lines" % count
+        print "Read %d records" % (count - 1)
 
-        print "zero count:", zero_count
-        print "bad stop (e.g., 9999) count:", bad_count
-        print "transfer count", transfer_count
-        print "wrong time count", wrong_time_count
-        print "wrong day count", wrong_day_count
-        print "bad_route_count", bad_route_count
-        print "filter count", filter_count
-        print "stop count", stop_count, valid_count, invalid_count
+        # print "bad stop (e.g., 9999) count:", bad_count
+        # print "transfer count", transfer_count
+        # print "wrong time count", wrong_time_count
+        # print "wrong day count", wrong_day_count
+        # print "bad_route_count", bad_route_count
+        # print "filter count", filter_count
+        # print "stop count", stop_count, valid_count, invalid_count
 
         print "Invalid Stops:"
         print "--------------"
@@ -348,6 +369,11 @@ class Runner(object):
             month = "jan"
         else:
             month = "sept"
+
+        print "SKIP FILE GENERATION" * 5
+        return
+
+        raise ValueError("temp stop in run")
 
         f_out  = open("%s_taps_per_pop.csv" % month, "w")
         f2_out = open("%s_taps_per_stop.csv" % month, "w")
@@ -391,17 +417,18 @@ class Runner(object):
 
     def display_date_data(self):
 
+        total_taps = 0
         items = []
         for date, taps in self._taps_per_date_dict.iteritems():
             items.append((date, taps))
-
+            total_taps += taps
         items.sort()
         for item in items:
             date = item[0]
             taps = item[1]
             date_object = self.get_date_object(date)
             day = self._iosday[date_object.isoweekday()]
-            print "%10s %10s  %8d" % (date, day, taps)
+            print "%10s, %10s,  %8d" % (date, day, taps)
 
         print "\n\n\n"
 
@@ -417,7 +444,44 @@ class Runner(object):
             day = item[1]
             print "%12s  %8d" % (day, taps)
 
+        print "\n"
+        print "total taps", total_taps
+
+    # def display_bad_stop_routes(self):
+    #
+    #     print "*"*80
+    #
+    #     route_number_name_map = {}
+    #
+    #     route_list = self._dataman.get_routes()
+    #     for item in route_list:
+    #         # print "this is a route", item
+    #
+    #         route_number = item.get_number()
+    #         route_name = item.get_name()
+    #         route_number_name_map[route_number] = route_name
+    #         count = self._bad_stop_routes.get(route_number)
+    #
+    #         print route_number, route_name, count
+    #
+    #     total_bad_stops = 0
+    #
+    #     for route_number, count in self._bad_stop_routes.iteritems():
+    #         route_name = route_number_name_map.get(route_number)
+    #         total_bad_stops += count
+    #         if route_name is None:
+    #             print route_number, "UNKNOWN", count
+    #
+    #     print "total bad stops", total_bad_stops
+
     def display_route_data(self):
+
+        print "*"*80
+
+        good_known = 0
+        bad_known = 0
+        good_unknown = 0
+        bad_unknown = 0
 
         if self._dataman is None:
             self._dataman = dataman_factory(self._dataset)
@@ -441,6 +505,13 @@ class Runner(object):
             name = route_number_name_map.get(route, "UNKNOWN")
             temp_list.append((taps, errors, route, name))
 
+            if name == "UNKNOWN":
+                good_unknown += taps
+                bad_unknown += errors
+            else:
+                good_known += taps
+                bad_known += errors
+
             # Add to taps per route *if route exists!*
             if taps_per_route.has_key(route):
                 taps_per_route[route] = taps
@@ -448,27 +519,55 @@ class Runner(object):
         temp_list.sort()
         temp_list.reverse()
 
+        f = open("bad_sept_stops2.csv", "w")
+
         for item in temp_list:
             taps = item[0]
             errors = item[1]
             route = item[2]
             name = item[3]
 
-            error_percent = 100.0 * errors / taps
+            error_percent = 100.0 * float(errors) / float(taps + errors)
 
-            print "ROUTE: %5d %-30s  taps: %5d errors: %5d (percent: %3.2f)" % (route, name, taps, errors, error_percent)
+#            print "ROUTE: %5d %-30s  taps: %5d errors: %5d (percent: %3.2f)" % (route, name, taps, errors, error_percent)
+            f.write("%d,%s,%d,%d,%3.2f\n" % (route, name, taps, errors, error_percent))
 
+        f.close()
 
+        print "good_known", good_known
+        print "bad_known", bad_known
+        print "good_unknown", good_unknown
+        print "bad_unknown", bad_unknown
         print "============="
+
         for route, taps in taps_per_route.iteritems():
             print route, taps
 
+    def display_fare_types(self):
+
+        x = []
+        for key, value in self._fare_type_dict.iteritems():
+            print key, "->", value
+            x.append((value, key))
+
+
+        x.sort()
+        x.reverse()
+
+        print "-------"
+        for item in x:
+            print item[1], ":", item[0]
+
     def rank_valid_stops(self):
+
+        total_good = 0
+        total_bad = 0
 
         stop_list = []
         for stop, value in self._valid_stop_dict.iteritems():
             #print stop, value
             stop_list.append((value, stop))
+            total_good += value
 
         stop_list = sorted(stop_list)
         stop_list.reverse()
@@ -478,13 +577,61 @@ class Runner(object):
             name = s.get_name()
             print "STOP: %d TAPS: %d NAME: %s" % (item[1], item[0], name)
 
+        # Make a CSV
+        count = 0
+        f=open("top_stops.csv", "w")
+        for item in stop_list:
+            s = self._dataman.get_stop(item[1])
+            name = s.get_name()
+            f.write("%d,%s,%d\n" % (item[1], name, item[0]))
+            count += 1
+            if count > 30:
+                break
+        f.close()
+
+
+        stop_list = []
+        for stop, value in self._invalid_stop_dict.iteritems():
+            #print stop, value
+            stop_list.append((value, stop))
+            total_bad += value
+
+        stop_list = sorted(stop_list)
+        stop_list.reverse()
+
+        for item in stop_list:
+            # s = self._dataman.get_stop(item[1])
+            # name = s.get_name()
+            print "INVALID STOP: %d TAPS: %d NAME: Unknown" % (item[1], item[0])
+
+        # Make a CSV
+        count = 0
+        f=open("invalid_stops.csv", "w")
+        for item in stop_list:
+            f.write("%d,Unknown,%d\n" % (item[1], item[0]))
+            count += 1
+            if count > 30:
+                break
+        f.close()
+
+        print "total godd", total_good
+        print "total bad", total_bad
+
+        print "total", total_good + total_bad
+
+        print "valid stop IDs", len(self._valid_stop_ids)
+
 if __name__ == "__main__":
 
     runner = Runner()
-
     runner.run("data/taps_sep_2018.csv")
 #     runner.run("data/taps_jan_2018.csv")
-    # runner.display_route_data()
-    runner.display_date_data()
+
+#    runner.display_fare_types()
+#    runner.display_route_data()
 
     runner.rank_valid_stops()
+
+    #raise ValueError('temp stop')
+
+    runner.display_date_data()
